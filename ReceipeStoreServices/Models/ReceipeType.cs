8bd42a061eb55187842cca378781bcac678bc512
deepthimudunuri07 <deepthiathrayasa@gmail.com>
2015-07-16 23:21:28 -0700

    adding model

---
+++ ReceipeStoreServices/Models/ReceipeType.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReceipeStoreServices.Models
+{
+    public class ReceipeType
+    {
+        private string _category;
+        private string _cuisine;
+        
+
+
+        public string Category
+        {
+            get
+            {
+                return _category;
+            }
+            set
+            {
+                _category = value;
+            }
+        }
+
+        public string Cuisine
+        {
+            get
+            {
+                return _cuisine;
+            }
+            set
+            {
+                _cuisine = value;
+            }
+        }
+
+        
+
+        public ReceipeType(string Category, string Cuisine, bool ishealthy)
+        {
+            this._category = Category;
+            this._cuisine = Cuisine;
+            
+        }
+    }
+}
