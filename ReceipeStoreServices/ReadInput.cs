027daa32b1ec7cf9feed6a1194a564e333f5a18d
deepthimudunuri07 <deepthiathrayasa@gmail.com>
2015-07-17 14:46:01 -0700

    addign a new file

---
+++ ReceipeStoreServices/ReadInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace ReceipeStoreServices
+{
+    public class ReadInput
+    {
+
+        private void ReadfromInput(string fileName)
+        {
+            
+        }
+    }
+}
